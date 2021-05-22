7d5c9948690d08c28a78067aea1f662d3104c1e9
nabenitez <nabenitez@uc.cl>
2021-05-22 11:50:10 -0400

    requests examples

---
+++ request2.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+const foo = async () => {
+  try {
+    const response = await fetch("https://fcm.googleapis.com/fcm/send", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "key=AAAAOK9Q5JY:APA91bHlHqdTp3gx-BngOckcuMGtl-PUDs7IbkFIw4L0kRJ2M2YKD5OG5lKu-N_ODDj8vSmHn1lDKoITVkW_wji94H7Mni9dPgM8rLEjgnk45w3y-i2a1_t1KNA1ofJ5fRjCEi38LYEQ",
+      },
+      method: "post",
+      body: JSON.stringify({
+        to: "dzcjOvu1AKuCt2ZPXCYm15:APA91bEOU_5wvD8vzrIWyqBJCh4rZ-04NQgWT3UElyTYAnF_FqTGm2ATWAtf05wIgDNxniCUwYTwnNgVm8GtStxTei8GZe-wuUixaAFDHoVDc52iPwBVs3ZQr7MkmeVqfFc-c9Q4XUUi",
+        notification: {
+          title: "AA",
+          body: "Yellowwwwwwwwwwwwww",
+        },
+      }),
+    });
+    console.log("Completed!", response);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+};
+foo();
