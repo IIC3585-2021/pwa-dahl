d564b789c629314bf975084efb7545a65056cc8e
beayancan <beayancan@uc.cl>
2021-05-23 22:31:01 -0400

    fetch

--- sw.js
+++ sw.js
@@ -51,29 +51,45 @@ self.addEventListener("activate", (evt) => {
 });
 
 // fetch events
-self.addEventListener("fetch", (evt) => {
-  if (evt.request.url.indexOf("firestore.googleapis.com") === -1) {
-    evt.respondWith(
-      caches
-        .match(evt.request)
-        .then((cacheRes) => {
-          return (
-            cacheRes ||
-            fetch(evt.request).then((fetchRes) => {
-              return caches.open(dynamicCacheName).then((cache) => {
-                cache.put(evt.request.url, fetchRes.clone());
-                // check cached items size
-                limitCacheSize(dynamicCacheName, 15);
-                return fetchRes;
-              });
-            })
-          );
-        })
-        .catch(() => {
-          if (evt.request.url.indexOf(".html") > -1) {
-            return caches.match("/pages/fallback.html");
-          }
-        })
+// self.addEventListener("fetch", (evt) => {
+//   if (evt.request.url.indexOf("firestore.googleapis.com") === -1) {
+//     evt.respondWith(
+//       caches
+//         .match(evt.request)
+//         .then((cacheRes) => {
+//           return (
+//             cacheRes ||
+//             fetch(evt.request).then((fetchRes) => {
+//               return caches.open(dynamicCacheName).then((cache) => {
+//                 cache.put(evt.request.url, fetchRes.clone());
+//                 // check cached items size
+//                 limitCacheSize(dynamicCacheName, 15);
+//                 return fetchRes;
+//               });
+//             })
+//           );
+//         })
+//         .catch(() => {
+//           if (evt.request.url.indexOf(".html") > -1) {
+//             return caches.match("/pages/fallback.html");
+//           }
+//         })
+//     );
+//   }
+// });
+
+self.addEventListener('fetch', event => {
+  //console.log('fetch called');
+  if (event.request.method === 'GET' && event.request.url.indexOf('https://jsonplaceholder.typicode.com') !== -1) {
+    event.respondWith(
+      caches.open(staticCacheName).then(cache => {
+        return fetch(event.request).then(response => {
+          cache.put(event.request, response.clone());
+          return response;
+        }).catch(error => {
+          return fetch(event.request);
+        });
+      })
     );
   }
 });
