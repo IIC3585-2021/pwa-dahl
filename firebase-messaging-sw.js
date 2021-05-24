bbb908ae7ae8292632630347aa2a3584d5e154b1
nabenitez <nabenitez@uc.cl>
2021-05-24 11:58:31 -0400

    fix: push notifications sw

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,40 +1,24 @@
 importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
 importScripts(
-    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+  "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js"
 );
 // For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
 importScripts(
-    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+  "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js"
 );
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-    messagingSenderId: "243459482774",
-    apiKey: "AIzaSyAtjnNNaxMd6Ga8k990SAFcj5vIH_aEnME",
-    projectId: "dahl-985a9",
-    appId: "1:243459482774:web:d24ff58d9fbdaf13314eb7",
-  });
+  messagingSenderId: "243459482774",
+  apiKey: "AIzaSyAtjnNNaxMd6Ga8k990SAFcj5vIH_aEnME",
+  projectId: "dahl-985a9",
+  appId: "1:243459482774:web:d24ff58d9fbdaf13314eb7",
+});
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function(payload) {
-    console.log(
-        "[firebase-messaging-sw.js] Received background message ",
-        payload,
-    );
-    // Customize notification here
-    const notificationTitle = "Background Message Title";
-    const notificationOptions = {
-        body: "Background Message body.",
-        icon: "/itwonders-web-logo.png",
-    };
-
-    return self.registration.showNotification(
-        notificationTitle,
-        notificationOptions,
-    );
-});
-
+self.addEventListener("install", (evt) => {});
+self.addEventListener("activate", (evt) => {});
