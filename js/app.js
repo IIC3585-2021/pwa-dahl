65ef5a8e0aeb23ddd2264f9c3482c9f4bd67c600
nabenitez <nabenitez@uc.cl>
2021-05-24 12:37:02 -0400

    fix7

--- js/app.js
+++ js/app.js
@@ -3,4 +3,8 @@ if ("serviceWorker" in navigator) {
     .register("./sw.js")
     .then((reg) => console.log("service worker registered"))
     .catch((err) => console.log("service worker not registered", err));
+  navigator.serviceWorker
+    .register("./firebase-messaging-sw.js")
+    .then((reg) => console.log("service worker registered"))
+    .catch((err) => console.log("service worker not registered", err));
 }
